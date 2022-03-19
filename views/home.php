325b6140cd952e5461a844b5fe10f144034b0457
Saul Tellez <saul.t.n.m@gmail.com>
2022-03-18 19:58:20 -0700

    Agregamos elementos en footer, navbar y home

--- views/home.php
+++ views/home.php
@@ -1,3 +1,31 @@
-
+<div class="row">
+    <h1>La pagina mas malota</h1>
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="">cheems</span>
+          <img src="https://c.tenor.com/OMDSFr1PLZcAAAAd/dogix-cheems.gif" style="width:570px;height:200px;"><br>
+        </div>
+        <div class="card-action">
+          <a href="#">Crees que funciona?</a>
+          <a href="#">Pues nelson xd</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="">KNY</span>
+          <img src="https://c.tenor.com/t_luUb2H7_EAAAAd/zenitsu-tanjiro.gif" style="width:570px;height:200px;"><br>
+        </div>
+        <div class="card-action">
+          <a href="#">Este no jala</a>
+          <a href="#">Este igual xd</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <script type="text/javascript" src="./scripts/home.js?v1.0.0"></script>
--- views/navbar.php
+++ views/navbar.php
@@ -1,3 +1,11 @@
-
+<nav>
+    <div class="nav-wrapper">
+      <a href="?view=registro" class="brand-logo" >Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Definir</a></li>
+        <li><a href="?view=login">Cerrar Sesion</a></li>
+      </ul>
+    </div>
+  </nav>
 
 <script src="./scripts/navbar.js?v1.0.0"></script>
